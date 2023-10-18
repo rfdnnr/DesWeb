df8bc5a889851cad1751e437db9ceb21782fffb8
rfdnnr <142801721+rfdnnr@users.noreply.github.com>
2023-10-18 08:56:34 -0300

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,45 @@
+const body = document.body;
+const div_container = document.createElement('div');
+div_container.id = 'container';
+body.appendChild(div_container);
+
+const cria_cartao = (entrada) => {
+    const container_atleta = document.createElement ('div');
+    const titulo = document.createElement('h3');
+    titulo.innerHTML = entrada.nome;
+    const imagem = document.createElement('img');
+    imagem.src = entrada.imagem;
+    const descricao = document.createElement('p');
+    descricao.innerHTML = entrada.descricao;
+
+    container_atleta.appendChild(titulo)
+    container_atleta.appendChild(imagem)
+    container_atleta.appendChild(descricao)
+
+    div_container.appendChild(container_atleta)
+}
+
+//atletas.forEach((atleta) => {cria_cartao(atleta)})
+
+/*
+let indice = 0;
+
+while (indice < atletas.length){
+    cria_cartao(atletas[indice]);
+    console.log(indice)
+    indice++;
+}
+
+*/
+
+for (let indice of atletas){
+    cria_cartao(indice);
+    console.log(indice)
+}
+
+
+/*for(let indice = 25; indice < atletas.length; indice--){
+    cria_cartao(atletas[indice]);
+    console.log(indice)
+}
+*/
